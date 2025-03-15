6a814e7bb991e98923e5b00666444f20ba365214
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-15 13:15:18 +0200

    Add: added MyHeap class with variables, contructors, isFull,isEmpty, howManyElements, resize

---
+++ src/datastr/MyHeap.java
@@ -0,0 +1,50 @@
+package datastr;
+
+public class MyHeap<Ttype> {
+	//mainīgie
+	private Ttype[] heap;
+	private final int DEFAULT_SIZE = 10;
+	private int size = DEFAULT_SIZE;
+	private int counter = 0;
+	
+	//bezargumenta konstruktors
+		public MyHeap() {
+			heap = (Ttype[])new Object[size];
+		}
+		
+		//argumenta konstruktors
+		public MyHeap(int inputSize) {
+			if(inputSize > 0) {
+				size = inputSize;
+			}
+			
+			heap = (Ttype[])new Object[size];	
+		}
+		
+		public boolean isFull()
+		{
+			return (counter == size);
+		}
+		
+		public boolean isEmpty() {
+			return (counter == 0);
+		}
+		
+		public int howManyElements() {
+			return counter;
+		}
+		
+		private void resize() {
+			size = (counter < 100) ? size * 2 : (int)(size * 1.5);
+			Ttype[] listNew = (Ttype[]) new Object[size];
+			
+			for(int i = 0; i < counter; i++) {
+				listNew[i] = heap[i];
+			}
+			heap = listNew;
+			System.gc();//izsaucam atkritumu savācēju, kas izdzēsīs mazā masīva elementus
+			
+		}
+		
+
+}
